5b9d3c1cf9ca0381b7e53013a92b7a33e3489836
Mauricio Abrahão <mauricioabrahaocr@gmail.com>
2022-10-29 11:47:06 -0300

    Project Commit

---
+++ src/main/java/br/com/ChallengeBackEnd102022/ChallengeBackEnd102022Application.java
@@ -0,0 +1,13 @@
+package br.com.ChallengeBackEnd102022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackEnd102022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackEnd102022Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ChallengeBackEnd102022/ChallengeBackEnd102022ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ChallengeBackEnd102022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackEnd102022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
