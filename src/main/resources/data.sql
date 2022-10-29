d7e8b6c5c6683145b0ed8c7b6c02edcec01442a3
Mauricio Abrahão <mauricioabrahaocr@gmail.com>
2022-10-29 13:42:18 -0300

    Database config for development and video model class

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO VIDEO(title, description, url) VALUES('Dad Battle - Friday Night Funkin OST', 'Composed by Kawai Sprite: https://twitter.com/kawaisprite From the game Friday Night Funkin Play the game on Newgrounds! ', 'https://www.youtube.com/watch?v=w0WyKTSuX4U');
+INSERT INTO VIDEO(title, description, url) VALUES('Soda City Funk', 'https://soundcloud.com/timlegend
+Thank you to Tim Legend for allowing me to use the music!
+(Obrigado a Tim Legend por permitir o uso da música!)', 'https://www.youtube.com/watch?v=zhIScvlFn2w');
+
